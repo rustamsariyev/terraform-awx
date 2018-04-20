0147017ab89f912d08a4a2b86e6dc82ec8f9d67b
Nick Adams <nickkadams@users.noreply.github.com>
2018-04-20 17:06:49 -0400

    Fixed pip upgrade issue

--- vsphere/awx.tf
+++ vsphere/awx.tf
@@ -131,7 +131,6 @@ resource "vsphere_virtual_machine" "vm" {
       "sudo apt-get update",
       "sudo apt-get install -y ansible",
       "sudo apt-get install -y python-pip",
-      "pip install --user --upgrade pip",
       "pip install --user docker",
       "git clone https://github.com/ansible/awx.git",
       "cd awx/installer",
